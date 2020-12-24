8c3488d5e788feac21ab5a8c01f97f0060cd26ac
David <56272724+ross0991@users.noreply.github.com>
2020-12-24 02:50:46 +0000

    Update countingCoins.cpp

--- countingCoins.cpp
+++ countingCoins.cpp
@@ -5,5 +5,27 @@
 
 using namespace std;
 
+#define MAXN 100
+vector<int> coins = {1, 3, 4};
+
 int main(){
+  int count[MAXN];
+  
+  for(int i=0; i<MAXN; i++){
+    count[i] = 0;
+  };
+  
+  count[0] = 1;
+  for(int x=0; x<MAXN; x++){
+    for(auto c : coins){
+      if(x -c >= 0){
+        count[x] += count[x-c];
+      }
+    }
+  }
+  
+  cout << count[5] << "\n";
+  
+  return 0;
+  
 };
